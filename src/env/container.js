13903b97d51a92411f6f89d73f25be5e7fdcf075
Luke Smith <lsmith@lucassmith.name>
2019-10-04 17:40:23 -0700

    Rename `readOnly` to `throws` to indicate what it *does*

--- src/env/container.js
+++ src/env/container.js
@@ -1,13 +1,13 @@
 import { container } from './configure-container';
 
-const readOnly = () => {
+const throws = () => {
     throw new Error('Use configure(METHODS) to configure dependency implementations');
 };
 
 const proxy = new Proxy(container, {
-    set: readOnly,
-    defineProperty: readOnly,
-    deleteProperty: readOnly,
+    set: throws,
+    defineProperty: throws,
+    deleteProperty: throws,
 });
 
 export default proxy;
